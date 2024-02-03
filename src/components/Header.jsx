e3e957b4fc0ec778087098818ce39a95d9eab82f
John <johntaykimani@gmail.com>
2024-02-03 14:03:25 +0300

    Completed Assignment

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header({onDarkModeClick, mode}) {
+  return (
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {mode} Mode
+        </button>
+    </header>
+  )
+}
+
+export default Header
